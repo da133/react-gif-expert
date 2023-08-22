7ce08e397311614a009273736089391c27c4a5e1
Daniela <danielaperezro65@gmail.com>
2023-08-22 11:03:30 -0600

    Actualización del testing

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { GifExpertApp } from "../src/GifExpertApp";
 
 describe('Puruebas en <GifExpertApp />', () => {
@@ -16,6 +16,49 @@ describe('Puruebas en <GifExpertApp />', () => {
         expect( screen.getByText('Greys Anatomy') ).toBeTruthy();
         expect( screen.getAllByText('Greys Anatomy').length ).toBe(1);
 
+    });
+ 
+    //Primero Debe agregar categorias nuevas:
+    test('should Add new categories', () => {
+
+        const newCategory = 'Friends';
+
+        render(<GifExpertApp />);
+        const input = screen.getByRole('textbox'); 
+        const form = screen.getByRole('form'); 
+ 
+        // Ahora disparo los eventos para agregar 3 categorias nuevas
+        fireEvent.input(input, { target: { value: newCategory } });
+        fireEvent.submit(form); //Agrega Friends
+ 
+        // Espero que se hallan agregado 1 categoría cuyo nombre aparece en etiqueta h3 más la categoría que 
+        // estaba por defecto
+        expect(screen.getAllByRole('heading', { level: 3 }).length).toBe(2);
+
+    });
+ 
+    // No debe agregar una categoría si ya existe
+    test('Should not add a repeated category', () => {
+
+        const newCategory = 'Friends';
+
+        render(<GifExpertApp />);
+        const input = screen.getByRole('textbox'); 
+        const form = screen.getByRole('form'); 
+ 
+        // Disparo los eventos para agregar una categoría
+        fireEvent.input(input, { target: { value: newCategory } });
+        fireEvent.submit(form);//agrega Friends
+ 
+        // Intento agregar la misma categoria
+        fireEvent.input(input, { target: { value: newCategory } });
+        fireEvent.submit(form); //intento agregar Friends nuevamente
+ 
+        // Espero que no agregue la misma categoría y sólo esté agregada la primera vez más la categoría que
+        // estaba por defecto
+        expect(screen.getAllByRole('heading', { level: 3 }).length).toBe(2);
+        
+        screen.debug();
     });
 
 });
